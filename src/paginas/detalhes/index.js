467269f1d7b51f4cc46fbaaffcb31a96ad05cf21
unknown <you@example.com>
2025-05-21 13:53:54 -0300

    Estilizando

--- src/paginas/detalhes/index.js
+++ src/paginas/detalhes/index.js
@@ -1,20 +1,20 @@
-import { View, Text, Image} from "react-native-web";
+import { View, Text, Image } from "react-native-web";
 import { useRoute } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./style";
 
-export default function Detalhes(){
+export default function Detalhes() {
     const route = useRoute();
     const navigation = useNavigation();
 
-    return(
+    return (
         <View style={styles.container}>
 
-            <Image style={styles.image} source={{uri:(route.params.imagem)}}/>
+            <Image style={styles.image} source={{ uri: (route.params.imagem) }} />
 
-            <Text>Nome: {route.params.titulo}</Text>
-            <Text>Nota: {route.params.nota}</Text>
-            <Text>Sinopse: {route.params.sinopse}</Text>
+                <Text style={styles.name}> Nome: {route.params.titulo}</Text>
+                <Text style={styles.nota}>Nota: {route.params.nota}</Text>
+                <Text style={styles.sinopse}>Sinopse: {route.params.sinopse}</Text>
 
         </View>
     )
--- src/paginas/detalhes/style.js
+++ src/paginas/detalhes/style.js
@@ -2,15 +2,32 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:'gray',
+        backgroundColor:'#a42022',
         height: '100%',
+        alignItems:'center'
     },
 
     image:{
         width:'260px',
         height:'380px',
-        borderRadius: 50,
+        borderRadius: 50
+    },
+
+    name:{
+       color: 'white', 
+       fontSize: '30px',
+    },
+
+    nota:{
+        color: 'white',
+    },
+
+    sinopse:{
+        color:'white',
+        width:'300px',
+        textAlign:'justify',
     }
+
 })
 
 export default styles;
